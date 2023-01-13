705fb457a0316ddc66650bbe8a500d14ef46379c
AayushG02 <aayush.girdhar2@gmail.com>
2023-01-13 23:59:43 +0530

    connected to local database

--- app.js
+++ app.js
@@ -27,28 +27,47 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, (err)=> {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("Inserted Items Successfully!")
-    }
-})
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-
-    res.render('list', {listTitle: "Today", newListItems: items});
+    Item.find({}, (err, foundItems)=>{
+        if(foundItems.length === 0) {
+            Item.insertMany(defaultItems, (err)=> {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log("Inserted Items Successfully!")
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", {listTitle: "Today", newListItems: foundItems});
+        }
+    });
 });
 
 app.post('/', (req, res) => {
-    var newitem = req.body.newItem;
-    items.push(newitem)
+    const newitem = req.body.newItem;
+    const userItem = new Item({
+        name: newitem
+    })
+    userItem.save();
     res.redirect('/')
 });
+app.post('/delete', (req, res)=> {
+    const checkedItem = req.body.deleteItem;
+    console.log(checkedItem)
+    Item.deleteOne({_id: checkedItem}, (err)=> {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Item Deleted!");
+        }
+    });
+    res.redirect('/');
+})
 app.listen(3000, () => {
     console.log('Server running on port 3000.');
 }); 
